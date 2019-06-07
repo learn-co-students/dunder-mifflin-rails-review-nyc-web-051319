80785df554a87fde99d0cab66a5e9a8a9b52e1a9
marycriv <maryecrivelli@gmail.com>
2019-06-07 11:37:26 -0400

    Pre seed

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,6 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dogs.all
+  end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+  validates :dog_id, uniqueness: true
+
+  def index
+    @employees = Employee.all
+  end
+
 end
