7e922776b62bc22d28e363128e4fcf1accfd2655
marycriv <maryecrivelli@gmail.com>
2019-06-07 11:47:50 -0400

    full_name method added

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,8 +2,8 @@ class Employee < ApplicationRecord
   belongs_to :dog
   validates :dog_id, uniqueness: true
 
-  def index
-    @employees = Employee.all
+  def full_name
+    self.first_name + " " + self.last_name
   end
 
 end
