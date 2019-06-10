bba3d8118737c4a9090fb1afcf267705afdab341
locky5 <daikevin19@yahoo.com>
2019-06-10 10:16:40 -0400

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,49 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.valid?
+      @employee.save
+      redirect_to @employee
+    else
+      flash[:message] = @employee.errors.full_messages[0]
+      redirect_to "/employees/new"
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.update(employee_params)
+    redirect_to @employee
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.delete
+    redirect_to "/employees"
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+  end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+  validates_uniqueness_of :alias
+  validates_uniqueness_of :title
+  belongs_to :dog
 end
